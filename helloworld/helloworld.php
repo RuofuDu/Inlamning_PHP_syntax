854d6e9bf39f7346fcf952818c76aeaaa6386db4
RuofuDu <92994154+RuofuDu@users.noreply.github.com>
2022-08-31 14:32:04 +0200

    Add files via upload

--- helloworld/helloworld.php
+++ helloworld/helloworld.php
@@ -16,11 +16,28 @@
         $huvudstader = array("Sverige" => "Stockholm", "Finland" => "Helsingfors", "Danmark" => "Köpenhamn", "Norge" => "Oslo", "Island" => "Reykjavik", "Estland" => "Tallinn", "Lettland" => "Riga", "Litauen" => "Vilnius");
 
         foreach ($huvudstader as $x => $x_value) {
+            echo "$x" . "s huvudstad är $x_value.";
+            echo "<br>";
+        }
+        echo "<br>";
+        foreach ($huvudstader as $x => $x_value) {
             echo "<tr><th>$x</th><th>$x_value</th></tr>";
         }
-
         ?>
 
+    </table>
+    <br>
+    <table>
+        <tr class="titel">
+            <th>Kurs</th>
+            <th>Lärare</th>
+        </tr>
+        <?php
+        $kurslarare = array("Webbserverprogrammering1" => "Holger", "Matematik5" => "Sevil", "MatematikSpec" => "Nikodemos", "Svenska3" => "Anna-Karin", "Historia1a1" => "Tommy", "Fysik2" => "Charles");
+        foreach ($kurslarare as $x => $x_value) {
+            echo "<tr><th>$x</th><th>$x_value</th></tr>";
+        }
+        ?>
     </table>
 
 </body>
